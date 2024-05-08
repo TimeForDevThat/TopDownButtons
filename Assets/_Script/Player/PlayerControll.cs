18736bef651bf5b4acd65a51362dd2b406cdd9a0
Michael <michaelvoov@yandex.ru>
2024-05-08 14:41:57 +0500

    Update
    New DASH

--- Assets/_Script/Player/PlayerControll.cs
+++ Assets/_Script/Player/PlayerControll.cs
@@ -1,28 +1,44 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.XR;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class PlayerControll : MonoBehaviour
 {
-    [SerializeField] private float walk;
-    [SerializeField] private float speed;
+    [SerializeField] private float _movementSpeed = 5f;
+    [SerializeField] private float _dashSpeed = 5000f;
+    [SerializeField] private float _dashTime = 2f;
 
     private Vector2 direction;
     private Rigidbody2D rb;
 
     private bool isf;
+    private bool _isDashing = true;
     private Type type;
 
     public enum Type {PC, Joystick}
     public Joystick joystick;
 
-    private void Start()
-        => rb = GetComponent<Rigidbody2D>();
+    private void Start() => rb = GetComponent<Rigidbody2D>();
 
     private void Update()
     {
-        GetComponent<Rigidbody2D>();
+        Dash();
+        InputDevice();
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.y = Input.GetAxisRaw("Vertical");
+    }
 
+    void FixedUpdate()
+        => rb.MovePosition(rb.position + direction * _movementSpeed * Time.fixedDeltaTime);
+
+    private void Flip()
+    {
+        isf = !isf;
+        transform.Rotate(0f, 180, 0f);
+    }
+
+    void InputDevice() {
         if (type == Type.PC)
         {
             joystick.gameObject.SetActive(false);
@@ -36,17 +52,19 @@ public class PlayerControll : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
-        else
-            rb.MovePosition(rb.position + direction * walk * Time.fixedDeltaTime);
+    void Dash() {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _isDashing == true) {
+            _isDashing = false;
+            Invoke("DashLock", _dashTime);
+            rb.velocity = new Vector2(0, 0);
+
+            if (rb.transform.localScale.x < 0)
+                rb.AddForce(Vector2.left * _dashSpeed);
+            else rb.AddForce(Vector2.right * _dashSpeed);
+        }
     }
 
-    private void Flip()
-    {
-        isf = !isf;
-        transform.Rotate(0f, 180, 0f);
+    void DashLock() { 
+        _isDashing = true;
     }
 }
