7b6f83289e848b405f5957a67a1841126e25a0cd
Michael <michaelvoov@yandex.ru>
2024-05-23 15:56:51 +0500

    Update Weapon.cs

--- Assets/_Script/Shooter/Weapon.cs
+++ Assets/_Script/Shooter/Weapon.cs
@@ -1,115 +1,120 @@
 using UnityEngine.UI;
 using UnityEngine;
 
-[AddComponentMenu("ScriptGun/Weapon")]
-public class Weapon : Sounds
-{
-    public GameObject bullet;
-    public float offset;
-
-    public bool isActive = true;
-
-    public GameObject Player;
-
-    [Header("ShotSpeed/ReloadSpeed")]
-    public float ShootSpeed;
-    public float ReloadSpeed;
-
-    [Header("Ammo")]
-    public int Cartridges; //��� ��������
-    public int CurCartridges; // ������
-
-    [Header("Text")] 
-    public Text maxCartridgesText;
-
-    [Header("Effect")]
-    public ParticleSystem shotEffect;
-    public Transform bulletSpawn; // point ��� �������
-
-    [Header("Timer")]
-    public float ReloadTimer = 1f; // ����� �����������(�� �������|�� ������)!!!!
-    public float ShootTimer = 0.0f; // ����� ��������(�� �������|�� ������)
-
-    void Update()
+namespace weapon {
+    [AddComponentMenu("ScriptGun/Weapon")]
+    public class Weapon : Sounds
     {
-        SpectorMouse();
-        CheckAmmoUiUpdate();
-        TouchButtonFireUpdate();
-        TouchButtonReloadUpdate();
-        AutoReload();
-        TimeUpdate();
-    }
+        public GameObject bullet;
+        public float offset;
 
-    void SpectorMouse() 
-    {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
-    }
+        public bool isActive = true;
 
-    void CheckAmmoUiUpdate()
-    {
-        maxCartridgesText.text = Cartridges + "/" + CurCartridges;
+        public GameObject Player;
 
-        if (Cartridges >= 1)
-            maxCartridgesText.color = new Color(255, 255, 255);
-        else
-            maxCartridgesText.color = new Color(255, 0, 0);
-    }
+        [Header("ShotSpeed/ReloadSpeed")]
+        public float ShootSpeed; // Вот тут
+        public float ReloadSpeed; // Вот тут
 
-    void TouchButtonFireUpdate()
-    {
-        if (Input.GetButtonDown("Fire1") & Cartridges > 0 & ReloadTimer <= 0 & ShootTimer <= 0 & isActive == true) {
-            Shoot();
-        }
-    }
+        [Header("Ammo")]
+        public int Cartridges; //��� ��������
+        public int CurCartridges; // ������
 
-    void ReloadCartridges()
-    {
-        ReloadTimer = ReloadSpeed;
+        [Header("Text")]
+        public Text maxCartridgesText;
 
-        isActive = false;
+        [Header("Effect")]
+        public ParticleSystem shotEffect;
+        public Transform bulletSpawn; // point ��� �������
 
-        Cartridges = CurCartridges;
-        CurCartridges -= 5;
-        YesShoot();
-    }
+        [Header("Timer")]
+        private float ReloadTimer = 0f; // не трогать не менять!!!!
+        private float ShootTimer = 0.0f; // не трогать не менять
 
-    void TouchButtonReloadUpdate()
-    {
-        if (Input.GetKey(KeyCode.R) && Cartridges == 0 & CurCartridges > 0 & ReloadTimer <= 0 & isActive == true)
+        void Update()
         {
-            ReloadCartridges();
+            SpectorMouse();
+            CheckAmmoUiUpdate();
+            TouchButtonFireUpdate();
+            TouchButtonReloadUpdate();
+            AutoReload();
+            TimeUpdate();
         }
-    }
 
-    void AutoReload()
-    {
-        if (Cartridges == 0 & CurCartridges > 0 & ReloadTimer <= 0 & isActive == true)
+        void SpectorMouse()
         {
-            ReloadCartridges();
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
         }
+
+        void CheckAmmoUiUpdate()
+        {
+            maxCartridgesText.text = Cartridges + "/" + CurCartridges;
+
+            if (Cartridges >= 1)
+                maxCartridgesText.color = new Color(255, 255, 255);
+            else
+                maxCartridgesText.color = new Color(255, 0, 0);
+        }
+
+        void TouchButtonFireUpdate()
+        {
+            if (Input.GetButtonDown("Fire1") & Cartridges > 0 & ReloadTimer <= 0 & ShootTimer <= 0 & isActive == true)
+            {
+                Shoot();
+            }
+        }
+
+        void ReloadCartridges()
+        {
+            ReloadTimer = ReloadSpeed;
+
+            isActive = false;
+
+            Cartridges = CurCartridges;
+            CurCartridges -= 5;
+            YesShoot();
+        }
+
+        void TouchButtonReloadUpdate()
+        {
+            if (Input.GetKey(KeyCode.R) && Cartridges == 0 & CurCartridges > 0 & ReloadTimer <= 0 & isActive == true)
+            {
+                ReloadCartridges();
+            }
+        }
+
+        void AutoReload()
+        {
+            if (Cartridges == 0 & CurCartridges > 0 & ReloadTimer <= 0 & isActive == true)
+            {
+                ReloadCartridges();
+            }
+        }
+
+        void TimeUpdate()
+        {
+            if (ShootTimer > 0)
+                ShootTimer -= Time.deltaTime;
+
+            if (ReloadTimer > 0)
+                ReloadTimer -= Time.deltaTime;
+        }
+
+        void Shoot()
+        {
+            shotEffect.Play();
+            ShootTimer = ShootSpeed;
+            Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+
+            Cartridges = Cartridges - 1;
+
+            PlaySounds(0);
+        }
+
+        void YesShoot()
+            => isActive = true;
     }
 
-    void TimeUpdate() {
-        if (ShootTimer > 0)
-            ShootTimer -= Time.deltaTime;
-
-        if (ReloadTimer > 0)
-            ReloadTimer -= Time.deltaTime;
-    }
-
-    void Shoot()
-    {
-        shotEffect.Play();
-        ShootTimer = ShootSpeed;
-        Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-
-        Cartridges = Cartridges - 1;
-
-        PlaySounds(0);
-    }
-
-    void YesShoot() 
-        => isActive = true;
 }
