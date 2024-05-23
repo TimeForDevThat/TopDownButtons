b91b89871e7ed576f610fa4e9ee6fb4d64aa2c8c
Michael <michaelvoov@yandex.ru>
2024-05-24 00:20:03 +0500

    Pausa

---
+++ Assets/_Script/Game/Manager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject Pausa;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            StopGame();
+            Pausa.SetActive(true);
+        }
+    }
+
+    public void Resume()
+        => Time.timeScale = 1.0f;
+
+    void StopGame()
+        => Time.timeScale = 0;
+}
