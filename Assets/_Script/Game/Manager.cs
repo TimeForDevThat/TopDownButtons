a009a845127728ddebe426edc86ab7eace9162a4
TimeForDevThat <antosha.07@mail.ru>
2024-05-24 14:01:18 +0500

    Pause menu now more convenient

--- Assets/_Script/Game/Manager.cs
+++ Assets/_Script/Game/Manager.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class Manager : MonoBehaviour
 {
     public GameObject Pausa;
+    public GameObject weapon;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            StopGame();
             Pausa.SetActive(true);
+            StopGame();
         }
     }
 
     public void Resume()
-        => Time.timeScale = 1.0f;
+    {
+        Time.timeScale = 1f;
+        weapon.GetComponent<Weapon>().enabled = true;
+    }
 
     public void StopGame()
-        => Time.timeScale = 0;
+    {
+        Time.timeScale = 0;
+        weapon.GetComponent<Weapon>().enabled = false;
+    }
 }
