293541e3143ca2c0021d29430f992748526a032b
Michael <michaelvoov@yandex.ru>
2024-06-30 01:52:56 +0500

    Update

--- Assets/_Script/Settings/Audio/volumeController.cs
+++ Assets/_Script/Settings/Audio/volumeController.cs
@@ -2,33 +2,37 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class volumeController : MonoBehaviour
-{
-    public string volumeParametr = "MasterVolume";
-    public AudioMixer mixer;
-    public Slider slider;
-
-    private float _volumeValue;
-    private const float _multiplier = 20f;
-
-    private void Awake()
+namespace volumeController{
+    public class volumeController : MonoBehaviour
     {
-        slider.onValueChanged.AddListener(HandSliderValueChanget);
-    }
+        public string volumeParametr = "MasterVolume";
+        public AudioMixer mixer;
 
-    private void HandSliderValueChanget(float value) { 
-        _volumeValue = Mathf.Log10(value) * _multiplier;
-        mixer.SetFloat(volumeParametr, _volumeValue);
-    }
+        [Space(5)]
+        [Header("UI")]
+        public Slider slider;
+        public Text textVolume;
 
-    void Start()
-    {
-        _volumeValue = PlayerPrefs.GetFloat(volumeParametr, Mathf.Log10(slider.value) * _multiplier);
-        slider.value = Mathf.Pow(10f, _volumeValue / _multiplier);
-    }
+        private float _volumeValue;
+        private const float _multiplier = 20f;
 
-    private void OnDisable()
-    {
-        PlayerPrefs.SetFloat(volumeParametr, _volumeValue);
+        private void Awake()
+            => slider.onValueChanged.AddListener(HandSliderValueChanget);
+
+        private void HandSliderValueChanget(float value)
+        {
+            _volumeValue = Mathf.Log10(value) * _multiplier;
+            mixer.SetFloat(volumeParametr, _volumeValue);
+            textVolume.text = Mathf.Round(f: value * 100) + "%";
+        }
+
+        void Start()
+        {
+            _volumeValue = PlayerPrefs.GetFloat(volumeParametr, Mathf.Log10(slider.value) * _multiplier);
+            slider.value = Mathf.Pow(10f, _volumeValue / _multiplier);
+        }
+
+        private void OnDisable()
+            => PlayerPrefs.SetFloat(volumeParametr, _volumeValue);
     }
 }
