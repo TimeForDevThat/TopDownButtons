abf19530f1d4260afa1a4cfc6ad61b412ef0e401
Michael <michaelvoov@yandex.ru>
2024-08-04 19:14:38 +0500

    Fixed

---
+++ Assets/_Script/game/player/PlayerSettings.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    public int what;
+    private bool _relvalue;
+    int BoolToInt(bool _relvalue)
+    {
+        if (_relvalue)
+            return 1;
+        else
+            return 0;
+    }
+
+    public void AutoReloadSetting()
+    {
+        PlayerPrefs.SetInt("AutomaticReload", BoolToInt(_relvalue));
+/*        if (PlayerPrefs.GetInt("AutomaticReload") == 1)
+        {
+            PlayerPrefs.SetInt("AutomaticReload", 0);
+        }
+
+
+        bool intToBool(int val)
+        {
+            if (val != 0)
+                return true;
+            else
+                return false;
+        }*/
+    }
+/*    private void Update()
+    {
+        what = PlayerPrefs.GetInt("AutomaticReload");
+    }*/
+}
